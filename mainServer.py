f6e92d9dff80dccbf88d9f6450b83916f59a2760
BlueDog31 <martsimu1@gmail.com>
2026-08-02 13:03:17 +0200

    Update mainServer.py

--- mainServer.py
+++ mainServer.py
@@ -2,3 +2,22 @@ import socket
 import numpy
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind("https://github.com/BlueDog31/Signalling_project.git")
+serverSocket.listen(1)
+clientSocket, clientAdress = serverSocket.accept()
+print(Definition: f"Tilkoblet fra: {client_address}")
+while True:
+    # Motta data fra klienten (opptil 1024 bytes) og dekod til tekst
+    data = client_socket.recv(1024).decode('utf-8')
+    if not data or data.lower() == 'hade':
+        print("Klienten koblet fra.")
+        break
+    print(f"Klienten sier: {data}")
+    
+    # Svar klienten
+    svar = input("Svar til klienten: ")
+    client_socket.send(svar.encode('utf-8'))
+
+# 6. Lukk forbindelsene
+client_socket.close()
+server_socket.close()
